98fcfaef5ab8b64c9ec93e42549a1b3d791b0f32
CarmenXP <xocuaposadas.carmen@gmail.com>
2020-06-15 20:22:38 -0500

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Home from './Home'
+import Home from './Components/Home'
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-    <Home />
+    
+    <Navbar/>
     
  </div>
   )
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Navbar(){
+    return(
+<nav id='navigator'>
+    <ul>
+        <li><a href="#">ícono</a></li>
+        <li><a href="#">Firefox</a></li>
+        <li><a href="#">Proyectos</a></li>
+        <li><a href="#">Desarrolladores</a></li>
+        <li><a href="#">Acerca de</a></li>
+        <li><button>Consigue una Firefox Account</button></li>
+        
+    </ul>
+
+</nav>
+    )
+}
+
+export default Navbar
